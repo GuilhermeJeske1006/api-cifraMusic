d2343d5cde6b0cbc296b51ac00856dd1cd9378c8
Guilherme jeske <guilhermeieski@gmail.com>
2023-12-05 11:18:01 -0300

    sends user creation email when creating the user

---
+++ app/Notifications/CreateUser.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CreateUser extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('Olá '. $this->user->name .' Seja muito bem vindo ao sistema.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Observers;
+
+use App\Jobs\SendEmailCreation;
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        SendEmailCreation::dispatch($user);
+    }
+
+
+}
