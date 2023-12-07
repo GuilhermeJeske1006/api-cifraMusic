962d28cffb2f10107d467896884e36704cd9e9b2
Guilherme jeske <guilhermeieski@gmail.com>
2023-12-07 09:50:59 -0300

    api-2: add middleware for authenticated in api.php

--- routes/api.php
+++ routes/api.php
@@ -21,18 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/music/highlight', HighlightController::class)->name('music.highlight');
-Route::post('/music/store', [MusicController::class, 'store'])->name('music.store');
-Route::get('/music', [MusicController::class, 'index'])->name('music.index');
-Route::get('/music/{music}', [MusicController::class, 'show'])->name('music.show');
-Route::put('/music/{music}/update', [MusicController::class, 'update'])->name('music.update');
-Route::delete('/music/{music}/destroy', [MusicController::class, 'destroy'])->name('music.destroy');
+Route::middleware('auth')->group(function () {
 
-Route::post('/singer/store', [SingerController::class, 'store'])->name('singer.store');
-Route::get('/singer', [SingerController::class, 'index'])->name('singer.index');
-Route::get('/singer/{singer}', [SingerController::class, 'show'])->name('singer.show');
-Route::put('/singer/{singer}/update', [SingerController::class, 'update'])->name('singer.update');
-Route::delete('/singer/{singer}/destroy', [SingerController::class, 'destroy'])->name('singer.destroy');
+    Route::get('/music/highlight', HighlightController::class)->name('music.highlight');
+    Route::post('/music/store', [MusicController::class, 'store'])->name('music.store');
+    Route::get('/music', [MusicController::class, 'index'])->name('music.index');
+    Route::get('/music/{music}', [MusicController::class, 'show'])->name('music.show');
+    Route::put('/music/{music}/update', [MusicController::class, 'update'])->name('music.update');
+    Route::delete('/music/{music}/destroy', [MusicController::class, 'destroy'])->name('music.destroy');
 
-Route::get('/webscriping/singer/search', SearchSingerController::class)->name('webscriping.singer');
-Route::get('/webscriping/music/search', SearchMusicController::class)->name('webscriping.music');
+    Route::post('/singer/store', [SingerController::class, 'store'])->name('singer.store');
+    Route::get('/singer', [SingerController::class, 'index'])->name('singer.index');
+    Route::get('/singer/{singer}', [SingerController::class, 'show'])->name('singer.show');
+    Route::put('/singer/{singer}/update', [SingerController::class, 'update'])->name('singer.update');
+    Route::delete('/singer/{singer}/destroy', [SingerController::class, 'destroy'])->name('singer.destroy');
+
+    Route::get('/webscriping/singer/search', SearchSingerController::class)->name('webscriping.singer');
+    Route::get('/webscriping/music/search', SearchMusicController::class)->name('webscriping.music');
+});
