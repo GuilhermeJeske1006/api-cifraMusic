63ff87c6993b95648eb0037770f1e4664f7b29fd
Guilherme jeske <guilhermeieski@gmail.com>
2023-12-08 17:59:19 -0300

    api-25: add selected music to pdf

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\Facebook\{CallbackController, RedirectController};
 use App\Http\Controllers\Auth\Google\{CallbackController as GoogleCallbackController, RedirectController as GoogleRedirectController};
+use App\Http\Controllers\Pdf\MusicPdfController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,5 +36,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/pdf/music/{music}', MusicPdfController::class)->name('pdf.music');
 
 require __DIR__ . '/auth.php';
